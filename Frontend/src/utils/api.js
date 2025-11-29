c324d1ee2c8ee072ef1aa556bab64d24dd9bfea5
vivek-c29 <vivekchauhan4773@gmail.com>
2025-11-29 09:36:12 +0530

    API utils file.

---
+++ Frontend/src/utils/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:5000/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Add a request interceptor to add the auth token to headers
+api.interceptors.request.use(
+  (config) => {
+    const userInfo = localStorage.getItem('userInfo');
+    if (userInfo) {
+      const { token } = JSON.parse(userInfo);
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
